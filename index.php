08a1375cb9464569724ac398595f422e0fc8b5ab
LeoDiD <148693769+LeoDiD@users.noreply.github.com>
2025-02-03 22:42:35 +0800

    add

--- index.php
+++ index.php
@@ -1,29 +1,32 @@
 <html>
 <head>
     <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
     <!-- Header -->
     <div class="header">
-    <span class="logo-text">SM</span>
-   <span class="logo-subtext">MARKETS</span>
-   <img src="images/sm.png" alt="SM logo" class="logo-image">
+        <span class="logo-text">SM</span>
+        <span class="logo-subtext">MARKETS</span>
+        <img src="images/sm.png" alt="SM logo" class="logo-image">
     </div>
     <!-- Main Content -->
     <div class="container">
         <!-- Sidebar -->
         <div class="sidebar">
             <ul>
-                <li>
-                    <i class="fas fa-shopping-cart"></i>
-                    Products
-                    <i class="fas fa-circle dot"></i>
+            <li>
+                <img src="images/product.png" alt="button" class="product-icon">
+                 <a href="pages/product.php" class="sidebar-button">
+                        Products
+                <img src="images/forward.png" alt="button" class="forward-icon">
+                </a>
                 </li>
             </ul>
         </div>
         <!-- Main Section -->
         <div class="main">
-            <h1>Products</h1>
+            <h1>ADMIN PANEL</h1>
         </div>
     </div>
 </body>
