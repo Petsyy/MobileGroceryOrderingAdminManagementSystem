d4265a3cd8095253efa2e891748587e7bb80b60b
LeoDiD <148693769+LeoDiD@users.noreply.github.com>
2025-02-03 19:51:28 +0800

    start of the final project

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <!-- Header -->
+    <div class="header">
+    <span class="logo-text">SM</span>
+   <span class="logo-subtext">MARKETS</span>
+   <img src="images/sm.png" alt="SM logo" class="logo-image">
+    </div>
+    <!-- Main Content -->
+    <div class="container">
+        <!-- Sidebar -->
+        <div class="sidebar">
+            <ul>
+                <li>
+                    <i class="fas fa-shopping-cart"></i>
+                    Products
+                    <i class="fas fa-circle dot"></i>
+                </li>
+            </ul>
+        </div>
+        <!-- Main Section -->
+        <div class="main">
+            <h1>Products</h1>
+        </div>
+    </div>
+</body>
+</html>
