d38bdd608074d8aee82f94bdc0aa0f8360b7edd8
Peter <pedi.arenas.up@phinmaed.com>
2025-02-24 00:45:42 +0800

    Revert "added user-profile in header only"
    This reverts commit c5dde6f7836525020183723d9f8e91945b4fffda.

--- index.php
+++ index.php
@@ -10,11 +10,9 @@
     <link rel="stylesheet" href="notification-bell/notification.css">
     
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-<header class="header">
-    <div class="logo-container">
+    <header class="header">
         <img src="./images/ez-mart.svg" alt="EZ Mart Logo" class="ez_logo" id="ezLogo" style="width: 60px;">
         <span class="logo-text"> Mart</span>
     </div>
